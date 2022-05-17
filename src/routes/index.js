1e172645e033fdfdff57865b874874312bf7cc39
Eniola Osabiya <eosabiya@gmail.com>
2022-05-17 11:54:41 +0100

    chore: added buyer and seller routes to path

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 
 router.use("/auth", require("./auth.route"));
+router.use("/buyer", require("./buyer.route"));
+router.use("/seller", require("./seller.route"));
 
 router.get("/", (req, res) => {
     return res.status(200).json({ message: "Hello world from hybr1d-assignment! :)" });

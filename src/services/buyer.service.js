94efd9258dae9cc4b92c76f00fc589804ea5a902
Eniola Osabiya <eosabiya@gmail.com>
2022-05-17 12:13:25 +0100

    chore: bug fix, missing import order for buyer service

--- src/services/buyer.service.js
+++ src/services/buyer.service.js
@@ -1,4 +1,5 @@
 const User = require("./../models/user.model");
+const Order = require("./../models/order.model");
 const Catalog = require("./../models/catalog.model");
 const CustomError = require("./../utils/custom-error");
 
